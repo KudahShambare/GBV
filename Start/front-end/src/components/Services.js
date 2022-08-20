655c38ca13f7a677817570fb373902bd4f989a7a
Kudakwashe Shambare <kudahshambare263@gmail.com>
2022-08-20 02:29:45 +0200

    added components

---
+++ Start/front-end/src/components/Services.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div id="services">
+      <h1>Fighting Gender Based Violence in Cape Town</h1>
+
+      <p>
+        {" "}
+        We need text from the organisation to put here. It must be a background
+        of what is gender based violence and its state in Cape tOWN A WELL AS
+        THE EFforts and measures put in place by the organization to fight GBV{" "}
+      </p>
+      <h2>Parners</h2>
+      <ul>
+        <li>Police</li>
+        <li>List all other partners</li>
+      </ul>
+      
+    </div>
+  );
+};
+export default Services;
--- Start/front-end/src/pages/Home.js
+++ Start/front-end/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Footer from "../components/Footer";
+import Services from "../components/Services";
 
 const Home=()=>{
     return(
         <><div className="pages">
-
+<Services/>
         </div><Footer /></>
     )
 }
