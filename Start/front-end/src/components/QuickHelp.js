11fff7dc8d3e0a87e6497607725127fdbe756f38
Kudakwashe Shambare <kudahshambare263@gmail.com>
2022-08-26 18:21:11 +0200

    Buttons added in quick start

--- Start/front-end/src/components/QuickHelp.js
+++ Start/front-end/src/components/QuickHelp.js
@@ -3,7 +3,13 @@ import React from "react";
 const QuickHelp=()=>{
     return(
         <div>
-
+<section>
+<p>You can report your case to our partners during </p>
+<button>Call The Police</button>
+</section>
+<section>
+<button id="emergency">Emergency</button>
+</section>
         </div>
     )
 }
