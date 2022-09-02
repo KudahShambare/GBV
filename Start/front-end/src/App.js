2bdbfd5ee517786b3793953686c56cf29549e7f8
Kudakwashe Shambare <kudahshambare263@gmail.com>
2022-09-02 23:46:13 +0200

    No Page Found

--- Start/front-end/src/App.js
+++ Start/front-end/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/Home";
-
+import NoPageFound from "./pages/NoPageFound";
 function App() {
   return (
     <>
@@ -11,6 +11,11 @@ function App() {
           <div id="page-body">
             <Switch>
               <Route exact path="/" component={Home} />
+                <Route exact path="/" component={Home} />
+                  <Route exact path="/" component={Home} />
+                    <Route exact path="/" component={Home} />
+
+                      <Route component={NoPageFound} />
             </Switch>
           </div>
         </div>
---
+++ Start/front-end/src/pages/NoPageFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NoPageFound=()=>{
+	return(
+
+<div>
+
+<h1> No Page Found </h1>
+</div>
+		)
+}
+export default NoPageFound;
