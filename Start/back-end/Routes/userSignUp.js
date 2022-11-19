1b0c47a3f00b90250374af009b126eea91ebf66c
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2022-11-19 17:22:42 +0200

    Spell correction

--- Start/back-end/Routes/userSignUp.js
+++ Start/back-end/Routes/userSignUp.js
@@ -23,11 +23,11 @@ router.post("/users", async (req, resp) => {
 		const hashedPassword = await bcrypt.hash(password, 10);
 		let socialWorkerQuery =
 			"INSERT INTO users (name,employee_id,password, date_created, role_id) VALUES ($1,$2,$3,$4, (SELECT id FROM roles WHERE name=$5))";
-		let funderQUery =
+		let funderQuery =
 			"INSERT INTO users (employee_id, password, date_created, role_id) VALUES($1,$2,$3, (SELECT id FROM roles WHERE name=$4))";
 		if (userName) {
 			client.query(
-				funderQUery,
+				funderQuery,
 				[userName, hashedPassword, dateCreated, userType],
 				(err, response) => {
 					if (err) throw err;
