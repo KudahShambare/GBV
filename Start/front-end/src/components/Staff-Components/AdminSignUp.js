1eb8caa3a1f82ca81836900b24fad3325c2ddb93
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2022-11-24 21:23:55 +0200

    Add in method and action to form

--- Start/front-end/src/components/Staff-Components/AdminSignUp.js
+++ Start/front-end/src/components/Staff-Components/AdminSignUp.js
@@ -1,31 +1,54 @@
 import  { Link } from "react-router-dom";
 
 const AdminSignUp = ()=>{
-	return(
-<div className="staff-forms">
+	return (
+		<div className="staff-forms">
+			<h4> Administrator Sign Up </h4>
+			<form
+				method="post"
+				action="https://plum-perfect-pronghorn.cyclic.app/users"
+			>
+				<input
+					type="text"
+					name="admin-name"
+					placeholder="First Name"
+					required
+				/>
+				<input
+					type="text"
+					name="admin-last-name"
+					placeholder="Last Name"
+					required
+				/>
+				<input type="email" name="admin-email" placeholder="Email" required />
+				<input
+					type="text"
+					name="admin-id"
+					placeholder="Touching Nations ID"
+					required
+				/>
+				<input
+					type="password"
+					name="admin-password"
+					placeholder="Password"
+					required
+				/>
 
-<h4> Administrator Sign Up </h4>
-<form>
-<input  type="text" name="admin-name" placeholder="First Name" required/>
-<input  type="text" name="admin-last-name" placeholder="Last Name" required/>
-<input type="email" name="admin-email" placeholder="Email" required />
-<input type="text" name="admin-id" placeholder="Touching Nations ID" required/>
-<input type="password" name="admin-password" placeholder="Password" required/>
+				<section className="staff-radios">
+					<label>Role</label>
+					<br />
+					<span>Manager</span>{" "}
+					<input type="radio" name="admin-role" value="female" required />
+					<span>Clerk</span>{" "}
+					<input type="radio" name="admin-role" value="male" required />
+				</section>
+				<button className="form-buttons">Sign Up </button>
+			</form>
 
-<section className="staff-radios">
-<label>Role</label><br/>
-<span>Manager</span> <input type="radio" name="admin-role" value="female"required/>
-<span>Clerk</span> <input type="radio" name="admin-role" value="male"required/>
-</section>
-<button className="form-buttons">Sign Up </button>
-
-
-</form>
-
-<Link to="/administrators-log-in">
- <span className="toggle-form"> Already have an account </span>
-</Link>
-</div>
-		)
+			<Link to="/administrators-log-in">
+				<span className="toggle-form"> Already have an account </span>
+			</Link>
+		</div>
+	);
 }
 export default AdminSignUp;
