aa397bc8c21372770569750509c41ad14407c218
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2022-11-24 21:34:28 +0200

    Updated input names to align with server variables

--- Start/front-end/src/components/Staff-Components/AdminSignUp.js
+++ Start/front-end/src/components/Staff-Components/AdminSignUp.js
@@ -8,40 +8,36 @@ const AdminSignUp = () => {
 				method="post"
 				action="https://plum-perfect-pronghorn.cyclic.app/users"
 			>
-				<input
-					type="text"
-					name="admin-name"
-					placeholder="First Name"
-					required
-				/>
-				<input
+				<input type="text" name="fullname" placeholder="First Name" required />
+				{/* <input
 					type="text"
 					name="admin-last-name"
 					placeholder="Last Name"
 					required
-				/>
+				/> */}
 				<input type="email" name="admin-email" placeholder="Email" required />
 				<input
 					type="text"
-					name="admin-id"
+					name="userName"
 					placeholder="Touching Nations ID"
 					required
 				/>
 				<input
 					type="password"
-					name="admin-password"
+					name="password"
 					placeholder="Password"
 					required
 				/>
-
-				<section className="staff-radios">
+				//Hi Kuda, staff role should be assigned by admin, not selected in
+				signup.
+				{/* <section className="staff-radios">
 					<label>Role</label>
 					<br />
 					<span>Manager</span>{" "}
 					<input type="radio" name="admin-role" value="female" required />
 					<span>Clerk</span>{" "}
 					<input type="radio" name="admin-role" value="male" required />
-				</section>
+				</section> */}
 				<button className="form-buttons">Sign Up </button>
 			</form>
 
