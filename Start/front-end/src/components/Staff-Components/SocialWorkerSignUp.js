31171fe599a62ffa1e7d63cad4dc8db9885e77bb
Ryan Smith <74462713+RyanS-Cpt@users.noreply.github.com>
2022-12-19 12:53:50 +0200

    Correct variable name

--- Start/front-end/src/components/Staff-Components/SocialWorkerSignUp.js
+++ Start/front-end/src/components/Staff-Components/SocialWorkerSignUp.js
@@ -1,35 +1,35 @@
 import { Link } from "react-router-dom";
 
 const SocialWorkerSignUp = () => {
-	return (
-		<div className="staff-forms">
-			<h4> Touching Nations Social Worker Sign Up</h4>
-			<form
-				method="post"
-				action="https://plum-perfect-pronghorn.cyclic.app/users"
-			>
-				<input type="text" name="fullname" placeholder="Full Name" required />
-				{/* <input type="text" name="social-worker-surname" placeholder="Last Name" required /> */}
-				<input
-					type="text"
-					name="employeeID"
-					placeholder="Employee ID"
-					required
-				/>
-				<input type="email" name="userName" placeholder="Email" required />
-				<input
-					type="password"
-					name="password"
-					placeholder="password"
-					required
-				/>
-				<button className="form-buttons"> Sign Up </button>
+  return (
+    <div className="staff-forms">
+      <h4> Touching Nations Social Worker Sign Up</h4>
+      <form
+        method="post"
+        action="https://plum-perfect-pronghorn.cyclic.app/users"
+      >
+        <input type="text" name="fullName" placeholder="Full Name" required />
+        {/* <input type="text" name="social-worker-surname" placeholder="Last Name" required /> */}
+        <input
+          type="text"
+          name="employeeID"
+          placeholder="Employee ID"
+          required
+        />
+        <input type="email" name="userName" placeholder="Email" required />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          required
+        />
+        <button className="form-buttons"> Sign Up </button>
 
-				<Link to="/social-worker-log-in">
-					<span className="toggle-form"> Already Have An Account </span>
-				</Link>
-			</form>
-		</div>
-	);
+        <Link to="/social-worker-log-in">
+          <span className="toggle-form"> Already Have An Account </span>
+        </Link>
+      </form>
+    </div>
+  );
 };
 export default SocialWorkerSignUp;
