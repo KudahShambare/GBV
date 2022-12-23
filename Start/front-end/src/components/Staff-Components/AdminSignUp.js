446e83c308ccf241ad50f4805f568ccde0f24d78
Ryan Smith <74462713+RyanS-Cpt@users.noreply.github.com>
2022-12-23 18:28:24 +0200

    Correct variable name for backend

--- Start/front-end/src/components/Staff-Components/AdminSignUp.js
+++ Start/front-end/src/components/Staff-Components/AdminSignUp.js
@@ -1,36 +1,36 @@
 import { Link } from "react-router-dom";
 
 const AdminSignUp = () => {
-	return (
-		<div className="staff-forms">
-			<h4> Administrator Sign Up </h4>
-			<form
-				method="post"
-				action="https://plum-perfect-pronghorn.cyclic.app/users"
-			>
-				<input type="text" name="fullname" placeholder="First Name" required />
-				{/* <input
+  return (
+    <div className="staff-forms">
+      <h4> Administrator Sign Up </h4>
+      <form
+        method="post"
+        action="https://plum-perfect-pronghorn.cyclic.app/users"
+      >
+        <input type="text" name="fullName" placeholder="First Name" required />
+        {/* <input
 					type="text"
 					name="admin-last-name"
 					placeholder="Last Name"
 					required
 				/> */}
-				<input type="email" name="userName" placeholder="Email" required />
-				{/* <input
+        <input type="email" name="userName" placeholder="Email" required />
+        {/* <input
 					type="text"
 					name="touchingNationsId"
 					placeholder="Touching Nations ID"
 					required
 				/> */}
-				<input
-					type="password"
-					name="password"
-					placeholder="Password"
-					required
-				/>
-				{/* Hi Kuda, staff role should be assigned by admin, not selected in
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+        />
+        {/* Hi Kuda, staff role should be assigned by admin, not selected in
 				signup. */}
-				{/* <section className="staff-radios">
+        {/* <section className="staff-radios">
 					<label>Role</label>
 					<br />
 					<span>Manager</span>{" "}
@@ -38,13 +38,13 @@ const AdminSignUp = () => {
 					<span>Clerk</span>{" "}
 					<input type="radio" name="admin-role" value="male" required />
 				</section> */}
-				<button className="form-buttons">Sign Up </button>
-			</form>
+        <button className="form-buttons">Sign Up </button>
+      </form>
 
-			<Link to="/administrators-log-in">
-				<span className="toggle-form"> Already have an account </span>
-			</Link>
-		</div>
-	);
+      <Link to="/administrators-log-in">
+        <span className="toggle-form"> Already have an account </span>
+      </Link>
+    </div>
+  );
 };
 export default AdminSignUp;
