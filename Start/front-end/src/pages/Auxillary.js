a39ee12270aa5a19dae6797758f38f5406e495aa
Ryan Smith <74462713+RyanS-Cpt@users.noreply.github.com>
2022-12-24 15:52:14 +0200

    Change inputs to match server

--- Start/front-end/src/pages/Auxillary.js
+++ Start/front-end/src/pages/Auxillary.js
@@ -3,41 +3,46 @@ import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 
-const Auxillary = ()=>{
+const Auxillary = () => {
+  //form info captured in backend
 
-	//form info captured in backend
+  return (
+    <>
+      <NavBar />
 
-	
-	return(
-		<>
-<NavBar/>
+      <div className="pages">
+        ]<section className="blank"> </section>
+        <div className="staff-forms">
+          <h4> Social Auxillary Worker Sign Up </h4>
+          <form>
+            <input
+              type="text"
+              name="fullName"
+              placeholder="Full name"
+              required
+            />
+            <input
+              type="text"
+              name="auxId"
+              placeholder="Employee ID/Email"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              required
+            />
+            <button className="form-buttons"> Sign Up </button>
 
-<div className="pages">]
-<section  className="blank"> </section>
-<div className="staff-forms" >
-<h4>  Social Auxillary Worker Sign Up </h4>
-		<form>
-		<input type="text" name="auxillary-name" placeholder="First Name" required />
-		<input type="text" name="auxillary-surname" placeholder="Last Name" required />
-		<input type="text" name="auxillary-id" placeholder="Employee ID" required />
-		<input type="email" name="auxillary-email" placeholder="Email" required />
-		<input type="password" name="auxillary-password" placeholder="password" required />
-         <button className="form-buttons" > Sign Up </button>
-
-         <Link to="/auxillary-social-worker-log-in">
-         <span className="toggle-form" > Already Have An Account </span>
-         </Link>
-
-
-		</form>
-		</div>
-		
-</div>
- <Footer/>
-
-</>
-
-
-	)
-}
+            <Link to="/auxillary-social-worker-log-in">
+              <span className="toggle-form"> Already Have An Account </span>
+            </Link>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 export default Auxillary;
