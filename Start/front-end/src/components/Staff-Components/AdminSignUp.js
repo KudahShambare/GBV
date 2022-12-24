ce930b9d0ebe2d2e530177b729c9c4ae1ecad3c5
Ryan Smith <74462713+RyanS-Cpt@users.noreply.github.com>
2022-12-24 15:55:50 +0200

    Remove clutter

--- Start/front-end/src/components/Staff-Components/AdminSignUp.js
+++ Start/front-end/src/components/Staff-Components/AdminSignUp.js
@@ -9,35 +9,13 @@ const AdminSignUp = () => {
         action="https://plum-perfect-pronghorn.cyclic.app/users"
       >
         <input type="text" name="fullName" placeholder="First Name" required />
-        {/* <input
-					type="text"
-					name="admin-last-name"
-					placeholder="Last Name"
-					required
-				/> */}
         <input type="email" name="userName" placeholder="Email" required />
-        {/* <input
-					type="text"
-					name="touchingNationsId"
-					placeholder="Touching Nations ID"
-					required
-				/> */}
         <input
           type="password"
           name="password"
           placeholder="Password"
           required
         />
-        {/* Hi Kuda, staff role should be assigned by admin, not selected in
-				signup. */}
-        {/* <section className="staff-radios">
-					<label>Role</label>
-					<br />
-					<span>Manager</span>{" "}
-					<input type="radio" name="admin-role" value="female" required />
-					<span>Clerk</span>{" "}
-					<input type="radio" name="admin-role" value="male" required />
-				</section> */}
         <button className="form-buttons">Sign Up </button>
       </form>
 
