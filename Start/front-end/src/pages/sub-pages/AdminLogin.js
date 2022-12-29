af1f139d49c49d271270cf8306bd0adcdf914b16
Ryan Smith <74462713+RyanS-Cpt@users.noreply.github.com>
2022-12-29 16:25:12 +0200

    Add in form method and action

--- Start/front-end/src/pages/sub-pages/AdminLogin.js
+++ Start/front-end/src/pages/sub-pages/AdminLogin.js
@@ -1,37 +1,48 @@
-
 import { Link } from "react-router-dom";
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
 
+const AdminLogin = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="pages">
+        <section className="blank"> </section>
 
-const AdminLogin = ()=>{
-	return(
-		<>
-		<NavBar/>
-<div className="pages">
-		<section className="blank">  </section>
+        <div className="staff-forms">
+          <h4> Administrator Log In </h4>
 
-<div className="staff-forms">
-
-<h4> Administrator Log In </h4>
-
-<form>
-<input type="email" name="admin-email" placeholder="Email" required />
-<input type="text" name="admin-id" placeholder="Touching Nations ID" required/>
-<input type="password" name="admin-password" placeholder="Password" required/>
-{/*Temporary Link to be replaced by page redirect*/}
-<Link to="/manager-dashboard">
-<button className="form-buttons" >Log In </button>
-</Link>
-
-</form>
-
-
-</div>
-
-</div>
-<Footer/>
-</>
-		)
-}
+          <form
+            method="post"
+            action="https://plum-perfect-pronghorn.cyclic.app/user-login"
+          >
+            <input
+              type="email"
+              name="admin-email"
+              placeholder="Email"
+              required
+            />
+            <input
+              type="text"
+              name="admin-id"
+              placeholder="Touching Nations ID"
+              required
+            />
+            <input
+              type="password"
+              name="admin-password"
+              placeholder="Password"
+              required
+            />
+            {/*Temporary Link to be replaced by page redirect*/}
+            <Link to="/manager-dashboard">
+              <button className="form-buttons">Log In </button>
+            </Link>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 export default AdminLogin;
