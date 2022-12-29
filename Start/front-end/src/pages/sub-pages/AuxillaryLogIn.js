84a7016c6756cf2bcd20cda39535445b4b21029d
Ryan Smith <74462713+RyanS-Cpt@users.noreply.github.com>
2022-12-29 16:27:32 +0200

    Add form attributes

--- Start/front-end/src/pages/sub-pages/AuxillaryLogIn.js
+++ Start/front-end/src/pages/sub-pages/AuxillaryLogIn.js
@@ -1,32 +1,38 @@
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
 
+const AuxillaryLogIn = () => {
+  return (
+    <>
+      <NavBar />
 
-const AuxillaryLogIn =()=>{
-	return(
-<>
-<NavBar/>
+      <div className="pages">
+        <section className="blank"> </section>
+        <div className="staff-forms" id="auxillary-login">
+          <h4> Social Auxillary Worker Log In </h4>
 
-<div className="pages">  
-<section className="blank"> </section>
- <div className="staff-forms" id="auxillary-login">
-<h4> Social Auxillary Worker Log In </h4>
-
-<form>
- <input type="email" name="auxillary-login-email" placeholder="Email" required/>
- <input type="password" name="auxillary-login-password" placeholder="Password" required/>
-<button className="form-buttons"> Log In </button>
-
-
-</form>
-
- </div>
-
-
-</div>
-<Footer/>
-</>
-
-		)
-}
+          <form
+            method="post"
+            action="https://plum-perfect-pronghorn.cyclic.app/user-login"
+          >
+            <input
+              type="email"
+              name="auxillary-login-email"
+              placeholder="Email"
+              required
+            />
+            <input
+              type="password"
+              name="auxillary-login-password"
+              placeholder="Password"
+              required
+            />
+            <button className="form-buttons"> Log In </button>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 export default AuxillaryLogIn;
