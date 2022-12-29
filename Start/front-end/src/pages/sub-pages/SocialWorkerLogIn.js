9515bf712f7e9e97e78019d1c939b469a6a3d5ba
Ryan Smith <74462713+RyanS-Cpt@users.noreply.github.com>
2022-12-29 15:44:35 +0200

    Add method and action to form

--- Start/front-end/src/pages/sub-pages/SocialWorkerLogIn.js
+++ Start/front-end/src/pages/sub-pages/SocialWorkerLogIn.js
@@ -1,30 +1,37 @@
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
 
-const SocialWorkerLogin = ()=>{
-	return(	
+const SocialWorkerLogin = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="pages">
+        <section className="blank"> </section>
 
-
-		<>
-<NavBar/>
-<div className="pages">
-		<section className="blank">  </section>
-
-<div className="staff-forms">  
-<h4> Touching Nations Social Worker Log In </h4>
-		<form>
- <input type = "email" name="social-login-email" placeholder="Email" required/>
- <input type = "password" name="social-login-password" placeholder="Password" required/>
-<button className="form-buttons">Log In </button>
- 
-		</form>
-</div>
-
-
-</div>
-<Footer/>
-		</>
-
-		)
-}
+        <div className="staff-forms">
+          <h4> Touching Nations Social Worker Log In </h4>
+          <form
+            method="post"
+            action="https://plum-perfect-pronghorn.cyclic.app/user-login"
+          >
+            <input
+              type="email"
+              name="social-login-email"
+              placeholder="Email"
+              required
+            />
+            <input
+              type="password"
+              name="social-login-password"
+              placeholder="Password"
+              required
+            />
+            <button className="form-buttons">Log In </button>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 export default SocialWorkerLogin;
